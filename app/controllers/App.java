6a8961ba3ba2d80d5255ac95e503f79a5ec6213a
Cédric Reginster <cederigo@gmail.com>
2011-11-10 01:32:42 +0100

    include js sdk so we can set our height prepare for serving FB channelUrl ( see FB dev docs )

--- app/controllers/App.java
+++ app/controllers/App.java
@@ -1,24 +1,27 @@
 package controllers;
 
+import com.sun.xml.internal.messaging.saaj.packaging.mime.Header;
+
 import play.mvc.Controller;
 
 /**
- * Facebook Application.
- * width: 760px.
+ * Facebook Application. width: 760px.
  */
 
 public class App extends Controller {
 
-  /*entry*/
+  /* entry */
   public static void index() {
     render();
   }
-  
-  
-  
-  
-  
-  
-  
-  
+
+  /**
+   * server facebook js api.
+   */
+  public static void channel() {
+    response.setHeader("Pragma", "public");
+    response.cacheFor("360d");
+    render();
+  }
+
 }
